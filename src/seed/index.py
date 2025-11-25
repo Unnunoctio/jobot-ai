d6ff7b5a680bf4380f5a1a2c459881bcaa9f94e2
Unnunoctio <unnunoctio.dev@gmail.com>
2025-11-25 17:27:28 -0300

    feat: :rocket: start new architecture

---
+++ src/seed/index.py
@@ -0,0 +1,22 @@
+import json
+
+import boto3
+
+TABLE_NAME = "JobotAIConfigTable"
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
+
+resp = table.scan(Limit=1)
+if resp.get("Items"):
+    print("Table already seeded")
+    exit(0)
+
+with open("src/seed/seed.json", "r") as f:
+    items = json.load(f)
+
+with table.batch_writer() as batch:
+    for item in items:
+        batch.put_item(Item=item)
+
+print("Seeded successfully")
